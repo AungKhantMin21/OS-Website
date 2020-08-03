2e5ad80d359709b6cdedb731fba25804502008f6
AungKhantMin21 <58725895+AungKhantMin21@users.noreply.github.com>
2020-08-03 20:28:16 +0630

    Home page and header and footer

---
+++ footer.php
@@ -0,0 +1,54 @@
+
+    <section class="mt-4 bg-dark text-light">
+        <div class="container p-4">
+            <div class="row mt-3">
+                <div class="col-md-3 text-center" style="border-right:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Company.com</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">About Us</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Contacts</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Terms & Conditions</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Privacy Policy</a><br>
+                </div>
+                <div class="col-md-3 text-center"  style="border-right:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Customer Service</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">Your Account</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Help</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">How to Buy</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Order Tracking</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Refund Policy</a><br>
+                </div>
+                <div class="col-md-3 text-center">
+                    <h5 style="font-weight:400;">Follow On</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">Facebook</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Instagram</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Twitter</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">LinkedIn</a><br>
+                </div>
+                <div class="col-md-3 ml-auto text-center"  style="border-left:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Contact Us</h5>
+                    <p style="font-weight: 300;"><i class="fas fa-map-marker-alt"></i> No.(123), Pan Chan Tower, <br>
+                        Pyay Road, Bahan, <br>
+                        Yangon <br>
+                        <b><i class="fas fa-phone-alt"></i> +95 123456789</b><br>
+                        <b><i class="fas fa-envelope"></i> company@domain.com</b></p>
+                </div>
+            </div>
+        </div>
+        <div class="text-center py-1" style="background:#1c1f21;">
+        © 2020.All right reserved.
+        </div>
+    </section>
+
+
+    
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.7/holder.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+    <script>
+    new WOW().init();
+    </script>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,279 @@
+<?php require "header.php";?>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-2">
+                <nav class="nav flex-column border shadow-sm">
+                    <a class="nav-link text-secondary border-bottom active" href="#">Home</a>
+                    <a class="nav-link text-secondary border-bottom" href="#">About Us</a>
+                    <a class="nav-link text-secondary border-bottom" href="#">How To Buy</a>
+                    <a class="nav-link text-secondary border-bottom active" href="#">Order Tracking</a>
+                    <div class="btn-group dropdown">
+                        <button type="button" class="btn btn-white text-left text-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Categories
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Electric Devices</a>
+                            <a class="dropdown-item" href="#">Fitness & Sport</a>
+                            <a class="dropdown-item" href="#">Health & Beauty</a>
+                            <a class="dropdown-item" href="#">Babies & Toys</a>
+                            <a class="dropdown-item" href="#">Groceries</a>
+                            <a class="dropdown-item" href="#">Women's Fashion</a>
+                            <a class="dropdown-item" href="#">Men's Fashion</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="col-md-7">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" style="height:300px;" src="https://laz-img-cdn.alicdn.com/images/ims-web/TB1cXbGbSslXu8jSZFuXXXg7FXa.jpg_1200x1200.jpg" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" style="height:300px;" src="https://laz-img-cdn.alicdn.com/images/ims-web/TB1K4qrPBr0gK0jSZFnXXbRRXXa.jpg_2200x2200Q100.jpg_.webp" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" style="height:300px;" src="https://laz-img-cdn.alicdn.com/images/ims-web/TB1DdZXbCslXu8jSZFuXXXg7FXa.jpg_2200x2200Q100.jpg_.webp" alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-3 d-none d-lg-block d-xl-block">
+                <div id="ads" class="px-3 py-1 shadow">
+                    <h3>Shopping With Us</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat optio, soluta ab veritatis autem qui ex, molestiae laborum cupiditate accusantium culpa ipsam quibusdam incidunt quod!</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 mt-2 ml-auto">
+                <img src="https://laz-img-cdn.alicdn.com/images/ims-web/TB1JuThMlr0gK0jSZFnXXbRRXXa.png" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 mt-5">
+                <div class="row">
+                    <div class="col-2" style="border-bottom:1px solid #f57100;">
+                        <h5 class="topic_heading">Trending Now</h5>
+                    </div>
+                    <div class="col-10 border-bottom"></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/96610bd6d12a167f8f3360f544a1515f.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/61f058b37cf2339614c93af79fdcc2f4.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/edffed69731ec35261428360a5cb1d18.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/d94eccea29e6d7752a5ed3cd11d1e8f5.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/0d81ed6fe7fdecc01ea8b693fbf02ffc.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/96610bd6d12a167f8f3360f544a1515f.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/96610bd6d12a167f8f3360f544a1515f.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/96610bd6d12a167f8f3360f544a1515f.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/96610bd6d12a167f8f3360f544a1515f.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/96610bd6d12a167f8f3360f544a1515f.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/96610bd6d12a167f8f3360f544a1515f.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-2 mt-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="card" style="max-width:189px;">
+                                <img src="https://static-01.shop.com.mm/p/96610bd6d12a167f8f3360f544a1515f.jpg_400x400q75-product.jpg_.webp"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adjustable Head Vented Helmet .... </p>
+                                    <p class="card-text" id="home_price">Ks 99999</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (112)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+    
+
+
+
+<?php require "footer.php"; ?>
