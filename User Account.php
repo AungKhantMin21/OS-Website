a31d5cd61a29968c9f8073e0d3497b3d4fcb9b66
MyatMinKhant23 <59755653+MyatMinKhant23@users.noreply.github.com>
2020-08-05 00:44:48 +0630

    Add files via upload

---
+++ User Account.php
@@ -0,0 +1,248 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>User Account</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.css'/>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.0.0/animate.css'/>
+    <style>
+        #signup{
+            border-color:#f57100;
+            color:#f57100;
+            border-radius:25px;
+            padding:3px 10px;
+        }
+        #signup:hover{
+            background:#f57100;
+            color:white;
+        }
+        #ads{
+            background:#FFCF75;
+            color:#1f374c;
+            border:1px solid #e6bb6a;
+            border-bottom:30px solid #e6bb6a;
+        }
+        .topic_heading{
+            font-family:"Lucida Console", Monaco, monospace;
+            color:#f57100;
+            font-weight:400;
+        }
+
+        #home_price{
+            color:#f57100;
+            text-decoration:underline;
+            font-size:18px;
+        }
+
+        #acc:hover{
+            color:orange;
+        }
+        .a{
+          font-size:18px;
+        }
+
+        p{
+          color:black;
+          font-size:15px;
+          font-family:;
+        }
+        h4{
+          align:center;
+        }
+        #acc1{
+          border-color:light;
+          font-size:15px;
+          font-style:arial;
+        }
+        #acc1:hover{
+          color:orange;
+          background-color:light;
+        }
+        hr{
+          width:200px;
+        }
+        img{
+          width:25px;
+          height:25px
+        }
+    </style>
+  </head>
+  
+  
+  <body>
+  <section style="background-color:#f57100;">
+    <div class="container">
+        <div class="row px-3 text-light" style="padding:1px 0px;">
+            <div class="col-4 text-center">
+                <i class="fab fa-facebook-f mx-1"></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-twitter mx-1"></i>
+                <i class="fab fa-linkedin-in"></i>
+                <i class="fab fa-viber mx-1"></i>
+            </div>
+            <div class="col-8 text-center" style="font-size:12px;">
+                © FREE SHIPPING FOR ALL ORDERS ABOVE $100.
+            </div>
+        </div>
+    </div>
+</section>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" style="border-bottom: 0.5px solid rgba(0, 0, 0, 0.281);">
+        <div class="container">
+            <a class="navbar-brand" href="#">LOGO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav" style="font-weight:450;">
+            <form class="form-inline ml-2 my-2 my-lg-0">
+                <div class="input-group">
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                    <div class="input-group-append">
+                    <button class="btn btn-secondary my-2 my-sm-0" id="search" type="submit"><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+            </form>
+            </ul>
+            <ul class="navbar-nav ml-auto" style="font-weight:450;">
+                <li class="nav-item ">
+                    <a href="#" class="nav-link mr-2"><i class="fas fa-shopping-cart" style="font-size:22px;"></i></a>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Help</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link"><button id="signup" class="btn btn-outline-secondary btn-sm">Sign Up</button></a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        
+    </nav>
+    
+      <div class="container text-light">
+        
+        <ul>
+          
+          <li>
+            <section class="mt-4 text-light">
+            <a href="#" id="acc"  class="text-secondary text-decoration-none border-right px-2">My Account <img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_account_circle_48px-512.png"></a>
+            <a href="#" id="acc"  class="text-secondary text-decoration-none border-right px-2">Recipe <img src="https://www.shareicon.net/data/2015/12/03/681739_paper_512x512.png"></a>
+            <a href="#" id="acc"  class="text-secondary text-decoration-none border-right px-2">Returns Order <img src="https://cdn0.iconfinder.com/data/icons/shopping-e-commerce-black-blue-version/33/package_return-512.png"></a>
+            <a href="#" id="acc"  class="text-secondary text-decoration-none border-right px-2">Cancel Order <img src="https://icon-library.com/images/cancel-icon-png/cancel-icon-png-5.jpg"></a>
+            <a href="#" id="acc"  class="text-secondary text-decoration-none px-2">Setting<img src="https://cdn3.iconfinder.com/data/icons/miscellaneous-15/100/Gear-512.png"></a></section>
+      </li> 
+        </ul>
+       
+      </div>
+      <h4 class="text-black text-center "style="font-weight:400;">My Account</h4><hr>
+      <section class="mt-4 bg-light text-light container p-4">
+     <div class="row">
+       
+        <div class="col-md-6 col-6 py-3" style="border-right:0.5px bg-light;">
+                    <p class="a"style="font-weight:300;">Full Name:</p>
+                    <p> Myat Min Khant</p>
+                   
+      </div>
+      <div class="col-md-6 col-6 py-3" style="border-right:0.5px bg-light;">
+                    <p class="a"style="font-weight:300;">Email:</p>
+                    <p>myat.khant23@gmail.com</p>
+      </div>
+      <div class="col-md-6 col-6 py-3" style="border-right:0.5px bg-light;">
+                    <p class="a"style="font-weight:300;">Mobile Phone:</p>
+                    <input type="number" id="number" placeholder="Enter your mobile no." width="100px"></input>
+      </div>
+     
+     
+      
+      <div class="col-md-6 col-6 py-3 " style="border-right:0.5px bg-light;">
+                    <p class="a"style="font-weight:300;">Gender</p>
+                    <input type="radio" id="male" name="gender" value="male">
+                    <label for="male"><p>Male</p></label>&nbsp;
+                    <input type="radio" id="female" name="gender" value="female">
+                    <label for="female"><p>Female</p></label>&nbsp;
+                    <input type="radio" id="other" name="gender" value="other">
+                    <label for="other"><p>Other</p></label>
+        </div>
+    
+  </div>
+  <div class="row">
+  <div class="col-md-6 col-6 py-3" style="border-right:0.5px bg-light;">
+      <p class="a"style="font-weight:300;">Address:</p>
+                   <input type="text" id="text" Placeholder="Enter Your Address.."></input>
+                   
+      </div>
+  
+  </div>
+  <div class="row">
+  <div class="col-md-6 col-6 py-3" style="border-right:0.5px bg-light;">
+                    <p class="a"style="font-weight:300;">Date of birth:</p>
+                   <input type="date" id="date" placeholder="Enter your Birthday"></input>
+      </div>
+  
+  </div>
+      
+  
+              </section>
+<footer>
+    <section class="mt-4 bg-dark text-light">
+        <div class="container p-4">
+            <div class="row mt-3">
+                <div class="col-md-3 text-center" style="border-right:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Company.com</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">About Us</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Contacts</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Terms & Conditions</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Privacy Policy</a><br>
+                </div>
+                <div class="col-md-3 text-center"  style="border-right:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Customer Service</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">Your Account</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Help</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">How to Buy</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Order Tracking</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Refund Policy</a><br>
+                </div>
+                <div class="col-md-3 text-center">
+                    <h5 style="font-weight:400;">Follow On</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">Facebook</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Instagram</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Twitter</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">LinkedIn</a><br>
+                </div>
+                <div class="col-md-3 ml-auto text-center"  style="border-left:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Contact Us</h5>
+                    <p style="font-weight: 300;"><i class="fas fa-map-marker-alt"></i> No.(123), Pan Chan Tower, <br>
+                        Pyay Road, Bahan, <br>
+                        Yangon <br>
+                        <b><i class="fas fa-phone-alt"></i> +95 123456789</b><br>
+                        <b><i class="fas fa-envelope"></i> company@domain.com</b></p>
+                </div>
+            </div>
+        </div>
+        <div class="text-center py-1" style="background:#1c1f21;">
+        © 2020.All right reserved.
+        </div>
+    </section>
+
+</footer>
+    
+
+  
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
