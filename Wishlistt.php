4be125a6241485c81eb85734ad88bf6ce6dc2c68
ShoonLaeMayLwin <59755760+ShoonLaeMayLwin@users.noreply.github.com>
2020-08-05 17:51:44 +0630

    Add files via upload

---
+++ Wishlistt.php
@@ -0,0 +1,215 @@
+<?php require "header.php";?>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-2">
+                <nav class="nav flex-column border shadow-sm">
+                    <a class="nav-link text-secondary border-bottom active" href="#">Home</a>
+                    <a class="nav-link text-secondary border-bottom" href="#">About Us</a>
+                    <a class="nav-link text-secondary border-bottom" href="#">How To Buy</a>
+                    <a class="nav-link text-secondary border-bottom active" href="#">Order Tracking</a>
+                    <div class="btn-group dropdown">
+                        <button type="button" class="btn btn-white text-left text-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Categories
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Electric Devices</a>
+                            <a class="dropdown-item" href="#">Fitness & Sport</a>
+                            <a class="dropdown-item" href="#">Health & Beauty</a>
+                            <a class="dropdown-item" href="#">Babies & Toys</a>
+                            <a class="dropdown-item" href="#">Groceries</a>
+                            <a class="dropdown-item" href="#">Women's Fashion</a>
+                            <a class="dropdown-item" href="#">Men's Fashion</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="col-md-7">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="wishlistheader">
+                        <h1 class="text-center">My Wishlist</h1> 
+                    </div>
+                <div class="row">
+        <div class="col-md-4 col-4 py-3" style="border-right:0.5px bg-light;">
+            <div class="select">
+                <select id="selectB" class="text-secondary text-decaration-none ">
+                    <option value="recentlyadd">Recently added</option>
+                    <option value="discount">Discount %</option>
+                    <option value="priceHtoL">Price: high to low</option>
+                    <option value="priceLtH">Price: low to high</option>
+                    <option value="brandAtZ">Brand A-Z</option>
+                    <option value="brandZtA">Brand Z-A</option>
+                    <option value="stock">Stock</option>
+                </select>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+                    <div class="col-md-4 col-4 py-3">
+                        <h5 class="topic_heading">Items</h5>
+                    </div>
+        </div>
+        <div class="row mt-3">
+                    <div class="col-md-4 col-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="" style="max-width:189px;">
+                                <img src="https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/0351ab62a1a2409c8aa0aae10126e90f_9366/FM1737_000_plp_model.jpg"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adidas Tee </p>
+                                    <p class="card-text" id="home_price">Ks 40000</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (87)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 col-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="" style="max-width:189px;">
+                                <img src="https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/0351ab62a1a2409c8aa0aae10126e90f_9366/FM1737_000_plp_model.jpg"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adidas Tee </p>
+                                    <p class="card-text" id="home_price">Ks 40000</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (87)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 col-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="" style="max-width:189px;">
+                                <img src="https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/0351ab62a1a2409c8aa0aae10126e90f_9366/FM1737_000_plp_model.jpg"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adidas Tee </p>
+                                    <p class="card-text" id="home_price">Ks 40000</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (87)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    </div>
+
+                    <div class="row mt-3">
+                    <div class="col-md-4 col-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="" style="max-width:189px;">
+                                <img src="https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/0351ab62a1a2409c8aa0aae10126e90f_9366/FM1737_000_plp_model.jpg"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adidas Tee </p>
+                                    <p class="card-text" id="home_price">Ks 40000</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (87)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 col-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="" style="max-width:189px;">
+                                <img src="https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/0351ab62a1a2409c8aa0aae10126e90f_9366/FM1737_000_plp_model.jpg"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adidas Tee </p>
+                                    <p class="card-text" id="home_price">Ks 40000</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (87)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 col-4">
+                        <a href="#" class="text-decoration-none text-dark text-left">
+                            <div class="" style="max-width:189px;">
+                                <img src="https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/0351ab62a1a2409c8aa0aae10126e90f_9366/FM1737_000_plp_model.jpg"  style="max-height:189px;" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">Adidas Tee </p>
+                                    <p class="card-text" id="home_price">Ks 40000</p>
+                                    <p class="card-text small"><i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i> (87)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="mt-4 bg-dark text-light">
+        <div class="container p-4">
+            <div class="row mt-3">
+                <div class="col-md-3 text-center" style="border-right:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Company.com</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">About Us</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Contacts</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Terms & Conditions</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Privacy Policy</a><br>
+                </div>
+                <div class="col-md-3 text-center"  style="border-right:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Customer Service</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">Your Account</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Help</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">How to Buy</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Order Tracking</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Refund Policy</a><br>
+                </div>
+                <div class="col-md-3 text-center">
+                    <h5 style="font-weight:400;">Follow On</h5>
+                    <a href="#" class="text-light" style="font-weight:300;">Facebook</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Instagram</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">Twitter</a><br>
+                    <a href="#" class="text-light" style="font-weight:300;">LinkedIn</a><br>
+                </div>
+                <div class="col-md-3 ml-auto text-center"  style="border-left:0.5px solid darkgrey;">
+                    <h5 style="font-weight:400;">Contact Us</h5>
+                    <p style="font-weight: 300;"><i class="fas fa-map-marker-alt"></i> No.(123), Pan Chan Tower, <br>
+                        Pyay Road, Bahan, <br>
+                        Yangon <br>
+                        <b><i class="fas fa-phone-alt"></i> +95 123456789</b><br>
+                        <b><i class="fas fa-envelope"></i> company@domain.com</b></p>
+                </div>
+            </div>
+        </div>
+        <div class="text-center py-1" style="background:#1c1f21;">
+        © 2020.All right reserved.
+        </div>
+    </section>
+
+
+    
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.7/holder.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+    <script>
+    new WOW().init();
+    </script>
+</body>
+</html>
